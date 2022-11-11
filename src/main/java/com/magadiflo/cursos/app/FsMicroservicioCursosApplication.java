441ccd7e1e0a5a3d6e3938b6c1816ffeca9146ac
Martín <magadiflo@gmail.com>
2022-11-11 10:34:11 -0500

    Creando librería commons para reutilizar entity Alumno
    Se agregó explicitamente el package a escanear para obtener las entidades, tanto de la librería como de este mismo proyecto

--- src/main/java/com/magadiflo/cursos/app/FsMicroservicioCursosApplication.java
+++ src/main/java/com/magadiflo/cursos/app/FsMicroservicioCursosApplication.java
@@ -2,9 +2,17 @@ package com.magadiflo.cursos.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/***
+ * @EntityScan({...}), le agregamos el package del entity de la librería commons
+ * alumnos y el package de este mismo proyecto, porque allí tenemos al Curso
+ *
+ */
+
 @EnableEurekaClient
+@EntityScan({ "com.magadiflo.commons.alumnos.models.entity", "com.magadiflo.cursos.app.models.entity" })
 @SpringBootApplication
 public class FsMicroservicioCursosApplication {
 
