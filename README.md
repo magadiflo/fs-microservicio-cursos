010830fd6bdc8a9762e2805513c1fbf439d6c0d9
Martín <magadiflo@gmail.com>
2022-11-10 19:40:17 -0500

    Proyecto base - Dependencias base: Spring Data JPA, Eureka Client, MySQL Driver, Spring Web

---
+++ README.md
@@ -0,0 +1,5 @@
+# Microservicios Spring Cloud Eureka & Angular Full-stack
+
+## Sección 4: Backend: Microservicio Cursos
+
+### 24. Creando microservicio cursos
---
+++ src/main/java/com/magadiflo/cursos/app/FsMicroservicioCursosApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.cursos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMicroservicioCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMicroservicioCursosApplication.class, args);
+	}
+
+}
